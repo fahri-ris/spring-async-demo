b2c7a885cf1245acafaa3995a9f9bd2254ba3c7f
fahri-ris <fahri.ris15@gmail.com>
2024-03-30 12:38:17 +0700

    start async demo

---
+++ src/test/java/com/demo/async/AsyncSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
